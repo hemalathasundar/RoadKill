f46cd7674dd76a2f689b8ae7923a78ed96196cc5
Chris S <mrshrinkray@gmail.com>
2016-10-06 20:25:11 +0100

    refactor: Some very simple Middleware

---
+++ src/Roadkill.Core/Text/TextMiddlewareBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFunc = System.Func<string, System.Threading.Tasks.Task>;
+
+namespace Roadkill.Core.Text
+{
+    public class TextMiddlewareBuilder
+    {
+        private readonly string _markdown;
+        private List<Middleware> _middleItems;
+
+        public TextMiddlewareBuilder(string markdown)
+        {
+            _markdown = markdown;
+            _middleItems = new List<Middleware>();
+        }
+
+        public void Use(Middleware middleware)
+        {
+            _middleItems.Add(middleware);
+        }
+
+        public string Execute()
+        {
+            string html = _markdown;
+            foreach (Middleware item in _middleItems)
+            {
+                html = item.Invoke(html);
+            }
+
+            return html;
+        }
+    }
+
+    public abstract class Middleware
+    {
+        public abstract string Invoke(string markup);
+    }
+}
