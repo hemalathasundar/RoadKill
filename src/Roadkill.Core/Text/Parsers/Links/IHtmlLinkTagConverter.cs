d677e3e29d25de8bf5dac1d971d435d9ef909474
Chris S <mrshrinkray@gmail.com>
2016-12-22 20:46:39 +0000

    links: special link converters

--- src/Roadkill.Core/Text/Parsers/Links/IHtmlLinkTagConverter.cs
+++
@@ -1,8 +0,0 @@
-namespace Roadkill.Core.Text.Parsers.Links
-{
-	public interface IHtmlLinkTagConverter
-	{
-		bool IsMatch(HtmlLinkTag htmlLinkTag);
-		HtmlLinkTag Convert(HtmlLinkTag htmlLinkTag);
-	}
-}
